ff8f32ccc3c2ba0b5b544b9bf9a73dea2880a262
ProfeSalva <142273583+ProfeSalva@users.noreply.github.com>
2025-10-17 11:43:40 -0300

    Create tpdatos.py

---
+++ tpdatos.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+import pandas as pd
+import duckdb as dd
+import numpy as np
+
+# Establecimientos: Data frame de Establecimientos Educativos del padròn del 2022
+
+
+columnas_ee = 'A:C,L,N,U:AA'
+"""
+A - Jurisdiccion
+B - Cueanexo
+C - Nombre
+L - Departamento
+N - Común
+U - Jardin maternal
+V - Jardin de infantes
+W - Primario
+X - Secundario
+Y - Secundario - INET
+Z - SNU
+AA - SNU - INET
+"""
+
+
+Establecimientos = pd.read_excel("/home/Estudiante/Descargas//2022_padron_oficial_establecimientos_educativos.xlsx", skiprows=6, usecols= columnas_ee)
+
+
+
+consultaSoloComunes = """
+                      SELECT *
+                      FROM Establecimientos
+                      WHERE "Común" = '1';
+
+                      """
+
+Establecimientos = dd.sql(consultaSoloComunes).df()
+
+elimino = """
+            SELECT Jurisdicción as Provincia, Departamento, "Nivel inicial - Jardín maternal" as Maternal,
+            "Nivel inicial - Jardín de infantes" as jardin, Primario, Secundario,
+            "Secundario - INET" as SecuInet, "SNU" as Snu, "SNU - INET" as SnuInet
+            FROM Establecimientos;
+          """
+
+Establecimientos = dd.sql(elimino).df()
+
+consultaMaternales =    """
+                            SELECT Provincia, Departamento, COUNT(Maternal) as Maternales, 
+                            COUNT(jardin) as Jardines, COUNT(Primario) as Primarias,
+                            COUNT(Secundario) as Secundarias, COUNT(SecuInet) as SecuInets,
+                            COUNT(Snu) as Snus, COUNT(SnuInet) as SnuInets
+                            FROM Establecimientos
+                            WHERE Maternal = '1' OR Jardin = '1' OR Primario = '1' OR
+                            Secundario = '1' OR SecuInet = '1' OR Snu = '1'
+                            OR SnuInet = '1'
+                            GROUP BY Departamento, Provincia;
+                        """
+df_EstablecimientosPorDepartamento = dd.sql(consultaMaternales).df()
+
+print(df_EstablecimientosPorDepartamento)
+
+
+consultaVerificoDepartamentos = """
+                                    SELECT COUNT(DISTINCT Departamento)
+                                    FROM df_EstablecimientosPorDepartamento;
+                                """
+verifico = dd.sql(consultaVerificoDepartamentos).df()
+
+print(verifico)
